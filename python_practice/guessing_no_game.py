9d433e921b6b36256faa7c5fd2f3732e4fdff008
Luis Ballinas <Cerebro@MacBook-Air-de-Maria.local>
2017-07-04 18:14:16 -0500

    ADDED: validation method

--- python_practice/guessing_no_game.py
+++ python_practice/guessing_no_game.py
@@ -1,1 +1,23 @@
 #A two player game about guessing a number.
+import random
+
+CONST_RANGE = 101
+active = True
+computer_election = 0
+
+def validation(player):
+    try:
+        print("Guess the number " + str(player))
+        print("Choice a number between 0 and " + str(CONST_RANGE - 1))
+        election = input("What is your choice ?")
+        if type(election) is int:
+            print("got it!")
+        else:
+            raise Exception ('ValueError')
+    except Exception as error:
+        print("Please enter a number")
+        validation(player)
+
+computer_election = random.randrange(CONST_RANGE)
+validation(1)
+validation(2)
